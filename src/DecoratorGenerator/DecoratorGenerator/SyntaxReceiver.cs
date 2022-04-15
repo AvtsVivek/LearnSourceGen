e0364be65056a93aa2f770c3843860a263a966ed
Vivekanand Koppula <vivekanand.koppula@mewurk.com>
2022-04-15 21:03:51 +0530

    Here we go

---
+++ src/DecoratorGenerator/DecoratorGenerator/SyntaxReceiver.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace LoggingGenerator
+{
+    public class SyntaxReceiver : ISyntaxReceiver
+    {
+        public HashSet<TypeDeclarationSyntax> TypeDeclarationsWithAttributes { get; } = new();
+        public HashSet<InterfaceDeclarationSyntax> InterfaceDeclarations { get; } = new();
+        public HashSet<ClassDeclarationSyntax> ClassDeclarations { get; } = new();
+        public HashSet<ClassDeclarationSyntax> ClassThatImplimentsInterface { get; } = new();
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is InterfaceDeclarationSyntax interfaceDeclaration)
+            {
+                InterfaceDeclarations.Add(interfaceDeclaration);
+            }
+
+            if (syntaxNode is TypeDeclarationSyntax declaration
+                //&& declaration.AttributeLists.Any()
+                )
+            {
+                TypeDeclarationsWithAttributes.Add(declaration);
+            }
+
+            if (syntaxNode is ClassDeclarationSyntax classDeclaration)
+            {
+                ClassDeclarations.Add(classDeclaration);
+                //if (HasInterface(classDeclaration))
+                //{ 
+                //    ClassThatImplimentsInterface.Add(classDeclaration);
+                //}
+            }
+
+
+
+        }
+
+        /// <summary>Indicates whether or not the class has a specific interface.</summary>
+        /// <returns>Whether or not the SyntaxList contains the attribute.</returns>
+        public bool HasInterface(ClassDeclarationSyntax source
+            //, string interfaceName
+            )
+        {
+            var interfaceName = "ISomeInterface";
+
+            IEnumerable<BaseTypeSyntax> baseTypes = source.BaseList.Types.Select(baseType => baseType);
+
+            // To Do - cleaner interface finding.
+            return baseTypes.Any(baseType => baseType.ToString() == interfaceName);
+        }
+
+    }
+}
---
+++ src/DecoratorGenerator/DecoratorGenerator/TrialGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace LoggingGenerator
+{
+    [Generator]
+    public class TrialGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var syntaxReceiver = (SyntaxReceiver)context.SyntaxReceiver;
+            var typeTargets = syntaxReceiver.TypeDeclarationsWithAttributes;
+            var interfaceTargets = syntaxReceiver.InterfaceDeclarations;
+            var classTargets = syntaxReceiver.ClassDeclarations;
+            var classThatImplimentInterfaces = syntaxReceiver.ClassThatImplimentsInterface;
+
+            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+            //var namespaceName = mainMethod.ContainingNamespace.ToDisplayString();
+
+            var compilation = context.Compilation;
+
+
+
+            var i = 0;
+            var finalSource = string.Empty;
+            finalSource = $@" // Auto-generated code
+using System.CodeDom.Compiler;
+using System.Runtime.CompilerServices;            
+using System;
+namespace {mainMethod.ContainingNamespace.ToDisplayString()}
+{{";
+
+            var source = string.Empty;
+            foreach (var targetTypeSyntax in classTargets)
+            {
+
+                var semanticModel = compilation.GetSemanticModel(targetTypeSyntax.SyntaxTree);
+                var targetType = semanticModel.GetDeclaredSymbol(targetTypeSyntax);
+                
+
+                i++;
+
+                source = $@"
+    [GeneratedCode(""{mainMethod.ContainingNamespace.ToDisplayString()}.A{i}"", ""x.x.x"")] // Check namespace and version info
+    [CompilerGenerated]
+    public class A{i}
+    {{
+/*
+{targetType} 
+The number of interfaces this type impliments is 
+{targetType.AllInterfaces.Count()}
+*/
+    }}
+";
+                finalSource = finalSource + source;
+            }
+            finalSource = finalSource + $@"
+}}";
+
+            context.AddSource($"A.g.cs", finalSource);
+        }
+
+
+    }
+}
